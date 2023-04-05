82a02ec7ffc8188bff027c02970cdda9ce6359d6
fatikhul36 <fatihmusyafa72@gmail.com>
2023-04-05 21:01:16 +0700

    update again

--- controllers/quiz.js
+++ controllers/quiz.js
@@ -18,3 +18,59 @@ exports.getAll = async(req, res) => {
     res.status(500).json({ message: 'Server error' });
   }
       };
+
+      //create: untuk menambah data ke dalam tabel quiz
+exports.create = async (req, res) => {
+
+  try {
+      const data = await Quiz.create(req.body)
+      res.json({
+          message: "quiz created succesfully.",
+          data: data,
+      })
+  } catch(error) {
+      res.status(500).json({
+          message: error.message,
+          data: null,
+      });
+  }
+}
+
+//update data quiz
+exports.update = async (req,res) => {
+  const id = req.params.id
+  try {
+      const quiz = await Quiz.findByPk(id, {rejectOnEmpty: true})
+      quiz.update(req.body,{
+          where: {id}
+      })
+      res.json({
+          message: "quizzes updated successfully",
+          data: quiz,
+      });
+  } catch (error) {
+      res.status(500).json({
+          message: error.message || "some error occured while retrieving quiz",
+          data: null,
+      });
+  }
+}
+
+//menghapus data quiz berdasarkan id
+exports.delete = async (req, res) => {
+  const id = req.params.id
+  try {
+      const quiz = await Quiz.findByPk(id,{rejectOnEmpty: true})
+
+      quiz.destroy()
+
+      res.json({
+          message: "quiz deleted successfully"
+      });
+  } catch (error) {
+      res.status(500).json({
+          message: error.message || "some error occured while retrieving quiz",
+          data: null,
+      });
+  }
+}
--- router/quiz.js
+++ router/quiz.js
@@ -2,5 +2,8 @@ const quizcontroller = require('../controllers/quiz');
 const router = require('express').Router();
 
 router.get('/',quizcontroller.getAll);
+router.post('/',quizcontroller.create);
+router.put('/:id',quizcontroller.update);
+router.delete('/:id',quizcontroller.delete);
 
 module.exports = router;
